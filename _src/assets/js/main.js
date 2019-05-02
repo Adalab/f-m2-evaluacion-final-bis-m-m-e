2be7e4341c34f5017eeb7f85c275ef8cd4d4a547
Megan Edwards <megmyfedwards@gmail.com>
2019-05-02 11:24:36 +0200

    Add Javascript to recognise radio choice

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,5 +2,44 @@
 
 //elements to use
 const options = document.querySelector('.options-list');
+const option1 = document.querySelector('#option1');
+const option2 = document.querySelector('#option2');
+const option3 = document.querySelector('#option3');
 const button = document.querySelector('.btn');
 const resultsBox = document.querySelector('.results');
+const placeholderImage = 'https://placehold.it/200x200';
+
+
+//function to print cards
+const printCards = number => {
+  const newCardList = document.createElement('ul');
+  newCardList.classList.add('card-list');
+
+  const newCard = document.createElement('li');
+  newCard.classList.add('card');
+
+  const newImage1 = document.createElement('img');
+  newImage1.classList.add('front-image');
+  newImage1.setAttribute('src', placeholderImage);
+
+  newCardList.appendChild(newCard);
+  resultsBox.appendChild(newCardList);
+};
+
+//function to create the correct number of cards
+const getCards = () => {
+  if (option1.checked === true) {
+    console.log(4);
+  }
+  else if (option2.checked === true) {
+    console.log(6);
+  }
+  else if (option3.checked === true) {
+    console.log(8);
+  }
+  else {
+    console.log('No number chosen');
+  }
+};
+
+button.addEventListener('click', getCards);
